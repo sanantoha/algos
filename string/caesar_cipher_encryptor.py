4d3212ed0fe671e9fe7bc1a58e4b27bb5d3575e4
sanantoha <sanantoha@gmail.com>
2021-09-11 14:13:04 +0400

    +1 task 11.09.2021

---
+++ string/caesar_cipher_encryptor.py
@@ -0,0 +1,16 @@
+
+# O(n) time | O(1) space
+def caesar_cipher_encryptor(string, key):
+    res = []
+    key %= 26
+    for idx in range(len(string)):
+        num = ord(string[idx])
+        new_num = num + key if num + key <= 122 else (num + key) % 122 + 96
+        res.append(chr(new_num))
+
+    return "".join(res)
+
+
+if __name__ == '__main__':
+    print(caesar_cipher_encryptor("xyz", 2))
+    print(caesar_cipher_encryptor("abc", 52))
