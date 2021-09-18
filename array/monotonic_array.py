fe46c92469a3c0158ee6f906b26759fb4d7c1b25
sanantoha <sanantoha@gmail.com>
2021-09-18 07:45:32 +0400

    +1 task 18.09.2021

---
+++ array/monotonic_array.py
@@ -0,0 +1,27 @@
+
+def monotonic_array(array):
+    if array is None or len(array) <= 2:
+        return True
+
+    direction = array[1] - array[0]
+
+    for i in range(2, len(array)):
+        if direction == 0:
+            direction = array[i] - array[i - 1]
+            continue
+
+        if break_direction(direction, array[i - 1], array[i]):
+            return False
+
+    return True
+
+
+def break_direction(direction, prev, curr):
+    diff = curr - prev
+    if direction > 0:
+        return diff < 0
+    return diff > 0
+
+
+if __name__ == '__main__':
+    print(monotonic_array([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]))
