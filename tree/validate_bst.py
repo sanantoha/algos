3c0293583efa0ab95a9c470a6d8c984c7504120a
sanantoha <sanantoha@gmail.com>
2021-09-27 14:49:33 +0300

    +1 task 27.09.2021

---
+++ tree/validate_bst.py
@@ -0,0 +1,29 @@
+from BST import BST
+
+
+def validate_bst(tree):
+    return validate(tree, float('-inf'), float('+inf'))
+
+
+def validate(tree, min_val, max_val):
+    if tree is None:
+        return True
+
+    if tree.value < min_val or tree.value >= max_val:
+        return False
+
+    return validate(tree.left, min_val, tree.value) and validate(tree.right, tree.value, max_val)
+
+
+if __name__ == '__main__':
+    root = BST(10)
+    root.left = BST(5)
+    root.left.left = BST(2)
+    root.left.left.left = BST(1)
+    root.left.right = BST(5)
+    root.right = BST(15)
+    root.right.left = BST(13)
+    root.right.left.right = BST(14)
+    root.right.right = BST(22)
+    print(validate_bst(root))
+    assert validate_bst(root)
