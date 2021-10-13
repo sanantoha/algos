d0a3fc6c6a219872f54dd65f8c07e33046cdf460
sanantoha <sanantoha@gmail.com>
2021-10-13 12:19:07 +0400

    +1 task 13.10.2021

--- search/binary_search.py
+++ search/binary_search.py
@@ -3,16 +3,18 @@
 def binary_search(array, target):
     left = 0
     right = len(array) - 1
-    while left < right:
+    while left <= right:
         mid = (left + right) // 2
         if target > array[mid]:
             left = mid + 1
+        elif target < array[mid]:
+            right = mid - 1
         else:
-            right = mid
+            return mid
 
-    return left if array[left] == target else -(left + 1)
+    return -(left + 1)
 
 
 if __name__ == '__main__':
     array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
-    print(binary_search(array, 51))
+    print(binary_search(array, 140))
