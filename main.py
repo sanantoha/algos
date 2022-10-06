ec61c62fe1b3ee150f74d1da27ca6cf2b56ba039
sanantoha <sanantoha@gmail.com>
2022-10-06 11:17:52 +0400

    +1 task 06.10.2022

--- main.py
+++ main.py
@@ -194,32 +194,13 @@ tasks_done = [
     "branch_sums.py",
     "height_balanced_binary_tree.py",
     "insertion_sort.py",
-    "bst_construction.py"
+    "bst_construction.py",
+    "merge_overlapping_intervals.py"
 ]
 
-class BST:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
 
-    def insert_rec(self, value):
-        return self
-
-    def insert(self, value):
-        return self
-
-    def contains(self, value):
-        return False
-
-    def contains_rec(self, value):
-        return False
-
-    def remove(self, value):
-        return self
-
-    def remove_rec(self, value):
-        return self
+def merge_overlapping_intervals(intervals):
+    pass
 
 
 if __name__ == '__main__':
@@ -227,40 +208,4 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    root = BST(10)
-    root.left = BST(5)
-    root.left.left = BST(2)
-    root.left.left.left = BST(1)
-    root.left.right = BST(5)
-    root.right = BST(15)
-    root.right.left = BST(13)
-    root.right.left.right = BST(14)
-    root.right.right = BST(22)
-
-    root.insert(12)
-    assert (root.right.left.left.value == 12)
-    #
-    # root.remove(10)
-    # assert (not root.contains(10))
-    # assert (root.value == 12)
-    #
-    # assert (root.contains(15))
-    #
-    root1 = BST(10)
-    root1.left = BST(5)
-    root1.left.left = BST(2)
-    root1.left.left.left = BST(1)
-    root1.left.right = BST(5)
-    root1.right = BST(15)
-    root1.right.left = BST(13)
-    root1.right.left.right = BST(14)
-    root1.right.right = BST(22)
-
-    root1.insert_rec(12)
-    assert (root1.right.left.left.value == 12)
-    #
-    # root1.remove_rec(10)
-    # assert (not root1.contains_rec(10))
-    # assert (root1.value == 12)
-    #
-    # assert (root1.contains_rec(15))
+    print(merge_overlapping_intervals([[1, 2], [3, 5], [4, 7], [6, 8], [9, 10]]))  # [[1, 2], [3, 8], [9, 10]]
