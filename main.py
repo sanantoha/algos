31c31434e5c1d37ce8f5e3dabf4704e20f2af482
sanantoha <sanantoha@gmail.com>
2022-10-14 12:13:48 +0400

    +1 task 14.10.2022

--- main.py
+++ main.py
@@ -202,55 +202,19 @@ tasks_done = [
     "longest_increasing_subsequence.py",
     "min_rewards.py",
     "suffix_trie_construction.py",
-    "remove_kth_node_from_end.py"
+    "remove_kth_node_from_end.py",
+    "single_cycle_check.py"
 ]
 
-# This is an input class. Do not edit.
-class LinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
 
-
-class StartLinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-
-linkedListClass = StartLinkedList
-# if hasattr(program, "LinkedList"):
-#     linkedListClass = program.LinkedList
-
-class LinkedList(linkedListClass):
-    def addMany(self, values):
-        current = self
-        while current.next is not None:
-            current = current.next
-        for value in values:
-            current.next = LinkedList(value)
-            current = current.next
-        return self
-
-    def getNodesInArray(self):
-        nodes = []
-        current = self
-        while current is not None:
-            nodes.append(current.value)
-            current = current.next
-        return nodes
-
-
-def remove_kth_node_from_end(head, k):
-    return head
+def single_cycle_check(arr):
+    pass
 
 if __name__ == '__main__':
     remain = list(set(tasks) - set(tasks_done))
     random.shuffle(remain)
     print(remain)
 
-    test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    expected = LinkedList(0).addMany([1, 2, 3, 4, 5, 7, 8, 9])
-    remove_kth_node_from_end(test, 4)
-    print(test.getNodesInArray())
-    assert test.getNodesInArray() == expected.getNodesInArray()
+    array = [2, 3, 1, -4, -4, 2]
+    print(single_cycle_check(array))
+    assert single_cycle_check(array)
