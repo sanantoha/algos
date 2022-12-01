fbfcb508c396f87cfd16ea68e0af5105a4aa5c0c
sanantoha <sanantoha@gmail.com>
2022-12-01 14:19:27 +0400

    +1 task 1.12.2022

--- main.py
+++ main.py
@@ -146,12 +146,36 @@ tasks_done = [
     "min_height_bst.py",
     "valid_starting_city.py",
     "branch_sums.py",
-    "reverse_words_in_string.py"
+    "reverse_words_in_string.py",
+    "max_sum_in_binary_tree.py"
 ]
 
-
-def reverseWordsInString(str):
+def maxPathSum(tree):
     pass
+
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, values, i=0):
+        if i >= len(values):
+            return
+        queue = [self]
+        while len(queue) > 0:
+            current = queue.pop(0)
+            if current.left is None:
+                current.left = BinaryTree(values[i])
+                break
+            queue.append(current.left)
+            if current.right is None:
+                current.right = BinaryTree(values[i])
+                break
+            queue.append(current.right)
+        self.insert(values, i + 1)
+        return self
 
 
 if __name__ == '__main__':
@@ -159,8 +183,7 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    input = "AlgoExpert is the best!"
-    expected = "best! the is AlgoExpert"
-    actual = reverseWordsInString(input)
+    test = BinaryTree(1).insert([2, 3, 4, 5, 6, 7])
+    actual = maxPathSum(test)
     print(actual)
-    assert actual == expected
+    assert actual == 18
