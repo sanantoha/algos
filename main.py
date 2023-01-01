731f86a5d2b09d806f8d9724a36b82ba13e9aabc
sanantoha <sanantoha@gmail.com>
2023-01-01 14:27:19 +0400

    +1 task 01.01.2023

--- main.py
+++ main.py
@@ -176,58 +176,21 @@ tasks_done = [
     "binary_search.py",
     "depth_first_search.py",
     "group_anagrams.py",
-    "remove_kth_node_from_end.py"
+    "remove_kth_node_from_end.py",
+    "zigzag_traverse.py"
 ]
 
-# This is an input class. Do not edit.
-class LinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
 
-
-def remove_kth_node_from_end(head, k):
+def zigzagTraverse(array):
     pass
 
-
-class StartLinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-
-linkedListClass = StartLinkedList
-# if hasattr(program, "LinkedList"):
-#     linkedListClass = program.LinkedList
-
-class LinkedList(linkedListClass):
-    def addMany(self, values):
-        current = self
-        while current.next is not None:
-            current = current.next
-        for value in values:
-            current.next = LinkedList(value)
-            current = current.next
-        return self
-
-    def getNodesInArray(self):
-        nodes = []
-        current = self
-        while current is not None:
-            nodes.append(current.value)
-            current = current.next
-        return nodes
 
 if __name__ == '__main__':
     remain = list(set(tasks) - set(tasks_done))
     random.shuffle(remain)
     print(remain)
 
-    test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    expected = LinkedList(0).addMany([1, 2, 3, 4, 5, 7, 8, 9])
-    remove_kth_node_from_end(test, 4)
-    print(test.getNodesInArray())
-    assert test.getNodesInArray() == expected.getNodesInArray()
-
-    # print(allPossibleFBT(7))
-    # print(allPossibleFBT1(5))
+    test = [[1, 3, 4, 10], [2, 5, 9, 11], [6, 8, 12, 15], [7, 13, 14, 16]]
+    actual = zigzagTraverse(test)
+    print(actual)
+    assert actual == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
