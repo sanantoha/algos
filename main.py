be6630af44955073b5699d00eeafbaa30c605efe
sanantoha <sanantoha@gmail.com>
2023-01-18 10:47:36 +0400

    +1 tasks task 18.01.2023

--- main.py
+++ main.py
@@ -192,62 +192,22 @@ tasks_done = [
     "palindrom_check.py",
     "all_paths_source_target.py",
     "cycle_in_graph.py",
-    "merge_binary_trees.py"
+    "merge_binary_trees.py",
+    "merging_linked_lists.py",
+    "zero_sum_subarray.py"
 ]
 
-from tree.BinaryTree import BinaryTree
 
-def mergeBinaryTrees(tree1, tree2):
-    pass
+def zeroSumSubarray(arr):
+    return False
 
-def mergeBinaryTreesIter(tree1, tree2):
-    pass
 
 if __name__ == '__main__':
     remain = list(set(tasks) - set(tasks_done))
     random.shuffle(remain)
     print(remain)
-
-    tree1 = BinaryTree(1)
-    tree1.left = BinaryTree(3)
-    tree1.left.left = BinaryTree(7)
-    tree1.left.right = BinaryTree(4)
-    tree1.right = BinaryTree(2)
-
-    tree2 = BinaryTree(1)
-    tree2.left = BinaryTree(5)
-    tree2.left.left = BinaryTree(2)
-    tree2.right = BinaryTree(9)
-    tree2.right.left = BinaryTree(7)
-    tree2.right.right = BinaryTree(6)
-
-    actual = mergeBinaryTrees(tree1, tree2)
-    assert actual.value == 2
-    assert actual.left.value == 8
-    assert actual.left.left.value == 9
-    assert actual.left.right.value == 4
-    assert actual.right.value == 11
-    assert actual.right.left.value == 7
-    assert actual.right.right.value == 6
-
-    tree3 = BinaryTree(1)
-    tree3.left = BinaryTree(3)
-    tree3.left.left = BinaryTree(7)
-    tree3.left.right = BinaryTree(4)
-    tree3.right = BinaryTree(2)
-
-    tree4 = BinaryTree(1)
-    tree4.left = BinaryTree(5)
-    tree4.left.left = BinaryTree(2)
-    tree4.right = BinaryTree(9)
-    tree4.right.left = BinaryTree(7)
-    tree4.right.right = BinaryTree(6)
-
-    actual = mergeBinaryTreesIter(tree3, tree4)
-    assert actual.value == 2
-    assert actual.left.value == 8
-    assert actual.left.left.value == 9
-    assert actual.left.right.value == 4
-    assert actual.right.value == 11
-    assert actual.right.left.value == 7
-    assert actual.right.right.value == 6
+    input = [4, 2, -1, -1, 3]
+    expected = True
+    actual = zeroSumSubarray(input)
+    print(actual)
+    assert actual == expected
