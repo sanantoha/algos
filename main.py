6190f86c4f22fa2ae473d75e50d825738d3c0292
sanantoha <sanantoha@gmail.com>
2023-01-20 10:47:44 +0400

    +1 tasks task 20.01.2023

--- main.py
+++ main.py
@@ -195,11 +195,12 @@ tasks_done = [
     "merge_binary_trees.py",
     "merging_linked_lists.py",
     "zero_sum_subarray.py",
-    "stable_internships.py"
+    "stable_internships.py",
+    "semordinal.py"
 ]
 
 
-def stableInternships(interns, teams):
+def semordnilap(words):
     pass
 
 
@@ -208,16 +209,7 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    interns = [[0, 1, 2], [0, 2, 1], [1, 2, 0]]
-    teams = [[2, 1, 0], [0, 1, 2], [0, 1, 2]]
-    expected = [[0, 1], [1, 0], [2, 2]]
-    actual = stableInternships(interns, teams)
-    print(actual)
-    assert len(actual) == len(expected)
-
-    for match in expected:
-        containsMatch = False
-        for actualMatch in actual:
-            if actualMatch[0] == match[0] and actualMatch[1] == match[1]:
-                containsMatch = True
-        assert containsMatch
+    input = ["desserts", "stressed", "hello"]
+    expected = [["desserts", "stressed"]]
+    actual = semordnilap(input)
+    print(actual)  # [['desserts', 'stressed']]
