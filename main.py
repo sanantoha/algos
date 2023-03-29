6d3c572c05e09667f0c22f6d760909653cffb551
sanantoha <sanantoha@gmail.com>
2023-03-29 10:38:53 +0400

    +1 tasks task 29.03.2023

--- main.py
+++ main.py
@@ -134,11 +134,12 @@ tasks_done = [
     "generate_document.py",
     "min_number_of_coins_for_change.py",
     "bst_traversal.py",
-    "valid_ip_addresses.py"
+    "valid_ip_addresses.py",
+    "word_ladder.py"
 ]
 
 
-def validIPAddresses(str):
+def ladderLength(beginWord, endWord, wordList):
     pass
 
 
@@ -147,20 +148,9 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    input = "1921680"
-    expected = [
-        "1.9.216.80",
-        "1.92.16.80",
-        "1.92.168.0",
-        "19.2.16.80",
-        "19.2.168.0",
-        "19.21.6.80",
-        "19.21.68.0",
-        "19.216.8.0",
-        "192.1.6.80",
-        "192.1.68.0",
-        "192.16.8.0",
-    ]
-    actual = validIPAddresses(input)
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+    actual = ladderLength(beginWord, endWord, wordList)
     print(actual)
-    assert actual == expected
+    assert actual == 5
