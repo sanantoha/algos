9555c356a7da2705b805256b8c4f1243cd2106f3
sanantoha <sanantoha@gmail.com>
2023-04-07 10:28:14 +0400

    +1 tasks task 07.04.2023

--- main.py
+++ main.py
@@ -144,11 +144,19 @@ tasks_done = [
     "find_closest_value_in_bst.py",
     "find_three_largest_numbers.py",
     "insertion_sort.py",
-    "number_of_ways_to_make_change.py"
+    "number_of_ways_to_make_change.py",
+    "merge_binary_trees.py"
 ]
 
 
-def number_of_ways_to_make_change(n, denoms):
+from tree.BinaryTree import BinaryTree
+
+
+def mergeBinaryTrees(tree1, tree2):
+    pass
+
+
+def mergeBinaryTreesIter(tree1, tree2):
     pass
 
 
@@ -157,5 +165,46 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    print(number_of_ways_to_make_change(6, [1, 5]))
-    assert number_of_ways_to_make_change(6, [1, 5]) == 2
+    tree1 = BinaryTree(1)
+    tree1.left = BinaryTree(3)
+    tree1.left.left = BinaryTree(7)
+    tree1.left.right = BinaryTree(4)
+    tree1.right = BinaryTree(2)
+
+    tree2 = BinaryTree(1)
+    tree2.left = BinaryTree(5)
+    tree2.left.left = BinaryTree(2)
+    tree2.right = BinaryTree(9)
+    tree2.right.left = BinaryTree(7)
+    tree2.right.right = BinaryTree(6)
+
+    actual = mergeBinaryTrees(tree1, tree2)
+    assert actual.value == 2
+    assert actual.left.value == 8
+    assert actual.left.left.value == 9
+    assert actual.left.right.value == 4
+    assert actual.right.value == 11
+    assert actual.right.left.value == 7
+    assert actual.right.right.value == 6
+
+    tree3 = BinaryTree(1)
+    tree3.left = BinaryTree(3)
+    tree3.left.left = BinaryTree(7)
+    tree3.left.right = BinaryTree(4)
+    tree3.right = BinaryTree(2)
+
+    tree4 = BinaryTree(1)
+    tree4.left = BinaryTree(5)
+    tree4.left.left = BinaryTree(2)
+    tree4.right = BinaryTree(9)
+    tree4.right.left = BinaryTree(7)
+    tree4.right.right = BinaryTree(6)
+
+    actual = mergeBinaryTreesIter(tree3, tree4)
+    assert actual.value == 2
+    assert actual.left.value == 8
+    assert actual.left.left.value == 9
+    assert actual.left.right.value == 4
+    assert actual.right.value == 11
+    assert actual.right.left.value == 7
+    assert actual.right.right.value == 6
