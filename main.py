55bd10fdf1f9759423041b00b07ad7e1822d5dfa
sanantoha <sanantoha@gmail.com>
2023-04-10 10:51:46 +0400

    +1 tasks task 10.04.2023

--- main.py
+++ main.py
@@ -146,11 +146,12 @@ tasks_done = [
     "insertion_sort.py",
     "number_of_ways_to_make_change.py",
     "merge_binary_trees.py",
-    "phone_number_mnemonic.py"
+    "phone_number_mnemonic.py",
+    "merge_overlapping_intervals.py"
 ]
 
 
-def phoneNumberMnemonics(phoneNumber):
+def merge_overlapping_intervals(intervals):
     pass
 
 
@@ -159,8 +160,4 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    phoneNumber = "1905"
-    expected = ["1w0j", "1w0k", "1w0l", "1x0j", "1x0k", "1x0l", "1y0j", "1y0k", "1y0l", "1z0j", "1z0k", "1z0l"]
-    actual = phoneNumberMnemonics(phoneNumber)
-    print(actual)
-    assert actual == expected
+    print(merge_overlapping_intervals([[1, 2], [3, 5], [4, 7], [6, 8], [9, 10]])) # [[1, 2], [3, 8], [9, 10]]
