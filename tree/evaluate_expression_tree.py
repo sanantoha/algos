aac12636aee1c6b673c70c44ee1cc22280c886fc
sanantoha <sanantoha@gmail.com>
2023-04-12 21:41:46 +0400

    +1 tasks task 12.04.2023

--- tree/evaluate_expression_tree.py
+++ tree/evaluate_expression_tree.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     tree = BinaryTree(-1)
     tree.left = BinaryTree(-2)
     tree.left.left = BinaryTree(-4)
-    tree.left.left.left = BinaryTree(2)
-    tree.left.left.right = BinaryTree(3)
+    tree.left.right = BinaryTree(2)
+    tree.left.left.left = BinaryTree(3)
     tree.left.left.right = BinaryTree(2)
 
     tree.right = BinaryTree(-3)
