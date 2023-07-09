76ef4d3dabfe3490a171940c2ec4736956fb6e34
sanantoha <sanantoha@gmail.com>
2023-07-09 09:08:14 +0400

    +1 tasks task 09.07.2023

--- array/missing_numbers.py
+++ array/missing_numbers.py
@@ -11,10 +11,31 @@ def missingNumbers(nums):
 
     return res
 
+# O(n) time | O(1) space
+def missingNumbers1(nums):
+    nums.append(1)
+    nums.append(1)
+
+    for i in range(len(nums) - 2):
+        num = nums[i]
+        nums[num - 1] *= -1
+
+    res = []
+    for i in range(len(nums)):
+        num = nums[i]
+        if num > 0:
+            res.append(i + 1)
+
+    return res
+
 
 if __name__ == '__main__':
     input = [4, 5, 1, 3]
     expected = [2, 6]
     actual = missingNumbers(input)
+    print(actual)
+    assert actual == expected
+
+    actual = missingNumbers1(input)
     print(actual)
     assert actual == expected
