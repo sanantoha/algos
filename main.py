722868a1176337adc80ccc8ae967dab47bdfc5ac
sanantoha <sanantoha@gmail.com>
2023-09-12 08:35:30 +0400

    +1 tasks task 12.09.2023

--- main.py
+++ main.py
@@ -144,15 +144,11 @@ tasks_done = [
     'validate_subsequence.py',
     'find_kth_largest_value_in_bst.py',
     'kadanes_distance.py',
-    'all_paths_source_target.py'
+    'number_of_ways_to_make_change.py'
 ]
 
 
-def allPathsSourceTarget(graph):
-    pass
-
-
-def allPathsSourceTarget1(graph):
+def number_of_ways_to_make_change(n, denoms):
     pass
 
 
@@ -161,14 +157,5 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    print(allPathsSourceTarget([[1, 2], [3], [3], []]))  # [[0, 2, 3], [0, 1, 3]]
-    print(allPathsSourceTarget(
-        [[4, 3, 1], [3, 2, 4], [3], [4], []]))  # [[0, 1, 4], [0, 1, 2, 3, 4], [0, 1, 3, 4], [0, 3, 4], [0, 4]]
-    print(allPathsSourceTarget([[4, 3, 1], [3, 2, 4], [], [4], []]))  # [[0, 1, 4], [0, 1, 3, 4], [0, 3, 4], [0, 4]]
-    print(allPathsSourceTarget([[2], [], [1]]))  # [[0, 2]]
-
-    print(allPathsSourceTarget1([[1, 2], [3], [3], []]))  # [[0, 2, 3], [0, 1, 3]]
-    print(allPathsSourceTarget1(
-        [[4, 3, 1], [3, 2, 4], [3], [4], []]))  # [[0, 1, 4], [0, 1, 2, 3, 4], [0, 1, 3, 4], [0, 3, 4], [0, 4]]
-    print(allPathsSourceTarget1([[4, 3, 1], [3, 2, 4], [], [4], []]))  # [[0, 1, 4], [0, 1, 3, 4], [0, 3, 4], [0, 4]]
-    print(allPathsSourceTarget1([[2], [], [1]]))  # [[0, 2]]
+    print(number_of_ways_to_make_change(6, [1, 5]))
+    assert number_of_ways_to_make_change(6, [1, 5]) == 2
