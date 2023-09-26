9b4459578eca033f45a64da4553beabe7b452136
sanantoha <sanantoha@gmail.com>
2023-09-26 08:57:14 +0400

    +1 tasks task 26.09.2023

--- main.py
+++ main.py
@@ -154,18 +154,12 @@ tasks_done = [
     'minimum_characters_for_words.py',
     'task_assignment.py',
     'first_duplicate_value.py',
-    'merge_binary_trees.py'
+    'merge_binary_trees.py',
+    'insertion_sort.py'
 ]
 
 
-from tree.BinaryTree import BinaryTree
-
-
-def mergeBinaryTrees(tree1, tree2):
-    pass
-
-
-def mergeBinaryTreesIter(tree1, tree2):
+def insertion_sort(arr):
     pass
 
 
@@ -174,46 +168,6 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    tree1 = BinaryTree(1)
-    tree1.left = BinaryTree(3)
-    tree1.left.left = BinaryTree(7)
-    tree1.left.right = BinaryTree(4)
-    tree1.right = BinaryTree(2)
-
-    tree2 = BinaryTree(1)
-    tree2.left = BinaryTree(5)
-    tree2.left.left = BinaryTree(2)
-    tree2.right = BinaryTree(9)
-    tree2.right.left = BinaryTree(7)
-    tree2.right.right = BinaryTree(6)
-
-    actual = mergeBinaryTrees(tree1, tree2)
-    assert actual.value == 2
-    assert actual.left.value == 8
-    assert actual.left.left.value == 9
-    assert actual.left.right.value == 4
-    assert actual.right.value == 11
-    assert actual.right.left.value == 7
-    assert actual.right.right.value == 6
-
-    tree3 = BinaryTree(1)
-    tree3.left = BinaryTree(3)
-    tree3.left.left = BinaryTree(7)
-    tree3.left.right = BinaryTree(4)
-    tree3.right = BinaryTree(2)
-
-    tree4 = BinaryTree(1)
-    tree4.left = BinaryTree(5)
-    tree4.left.left = BinaryTree(2)
-    tree4.right = BinaryTree(9)
-    tree4.right.left = BinaryTree(7)
-    tree4.right.right = BinaryTree(6)
-
-    actual = mergeBinaryTreesIter(tree3, tree4)
-    assert actual.value == 2
-    assert actual.left.value == 8
-    assert actual.left.left.value == 9
-    assert actual.left.right.value == 4
-    assert actual.right.value == 11
-    assert actual.right.left.value == 7
-    assert actual.right.right.value == 6
+    xs = [9, 8, 1, 5, 6, 7, 8, 3, 9, 7, 5, 3]
+    # xs = [5,2,4,1]
+    print(insertion_sort(xs))
