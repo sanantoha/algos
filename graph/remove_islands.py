ae82fa76b2c26a7c049583b7fd0425376087b80f
sanantoha <sanantoha@gmail.com>
2023-10-26 11:46:33 +0400

    +1 tasks task 26.10.2023

--- graph/remove_islands.py
+++ graph/remove_islands.py
@@ -1,40 +1,44 @@
 
-# O(w * h) time | O(1) space
+# O(w * h) time | O(w * h) space
 def remove_islands(matrix):
     if matrix is None or len(matrix) == 0:
         return matrix
 
     for row in range(len(matrix)):
-        if matrix[row][0] == 1:
-            dfs(matrix, row, 0)
-        if matrix[row][-1] == 1:
-            dfs(matrix, row, len(matrix[row]) - 1)
+        for col in range(len(matrix)):
+            row_is_border = row == 0 or row == len(matrix) - 1
+            col_is_border = col == 0 or col == len(matrix[row]) - 1
+            is_border = row_is_border or col_is_border
 
-    for col in range(len(matrix[0])):
-        if matrix[0][col] == 1:
-            dfs(matrix, 0, col)
-        if matrix[-1][col] == 1:
-            dfs(matrix, len(matrix) - 1, col)
+            if not is_border or matrix[row][col] != 1:
+                continue
+
+            dfs(matrix, row, col)
 
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
-            if matrix[row][col] == '*':
-                matrix[row][col] = 1
-            elif matrix[row][col] == 1:
+            if matrix[row][col] == 1:
                 matrix[row][col] = 0
+            elif matrix[row][col] == 2:
+                matrix[row][col] = 1
 
     return matrix
 
 
 def dfs(matrix, row, col):
-    if matrix[row][col] != 1:
-        return
+    stack = [(row, col)]
 
-    matrix[row][col] = '*'
-    neighbors = get_neighbors(matrix, row, col)
-    for (next_row, next_col) in neighbors:
-        if matrix[next_row][next_col] == 1:
-            dfs(matrix, next_row, next_col)
+    while stack:
+        (curr_row, curr_col) = stack.pop()
+
+        matrix[curr_row][curr_col] = 2
+        neighbors = get_neighbors(matrix, curr_row, curr_col)
+
+        for (next_row, next_col) in neighbors:
+            if matrix[next_row][next_col] != 1:
+                continue
+
+            stack.append((next_row, next_col))
 
 
 def get_neighbors(matrix, row, col):
