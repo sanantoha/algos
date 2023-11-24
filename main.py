462c12880a83bf19cbd096d458f10589ae9a2fd6
sanantoha <sanantoha@gmail.com>
2023-11-24 08:46:47 +0400

    +1 tasks task 24.11.2023

--- main.py
+++ main.py
@@ -189,11 +189,12 @@ tasks_done = [
     'class_photos.py',
     'number_of_islands.py',
     'sorted_squared_array.py',
-    'balanced_brackets.py'
+    'balanced_brackets.py',
+    'valid_ip_addresses.py'
 ]
 
 
-def balancedBrackets(string):
+def validIPAddresses(string):
     pass
 
 
@@ -202,6 +203,20 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    assert balancedBrackets("(a)")
-    assert balancedBrackets("([])")
-    assert balancedBrackets("([])(){}(())()()")
+    input = "1921680"
+    expected = [
+        "1.9.216.80",
+        "1.92.16.80",
+        "1.92.168.0",
+        "19.2.16.80",
+        "19.2.168.0",
+        "19.21.6.80",
+        "19.21.68.0",
+        "19.216.8.0",
+        "192.1.6.80",
+        "192.1.68.0",
+        "192.16.8.0",
+    ]
+    actual = validIPAddresses(input)
+    print(actual)
+    assert actual == expected
