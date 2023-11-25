e55ed86bc52f42069b7144aaa86d9755545cab31
sanantoha <sanantoha@gmail.com>
2023-11-25 10:56:22 +0400

    +1 tasks task 25.11.2023

--- main.py
+++ main.py
@@ -190,11 +190,12 @@ tasks_done = [
     'number_of_islands.py',
     'sorted_squared_array.py',
     'balanced_brackets.py',
-    'valid_ip_addresses.py'
+    'valid_ip_addresses.py',
+    'palindrom_check.py'
 ]
 
 
-def validIPAddresses(string):
+def is_palindrome(str):
     pass
 
 
@@ -203,20 +204,4 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    input = "1921680"
-    expected = [
-        "1.9.216.80",
-        "1.92.16.80",
-        "1.92.168.0",
-        "19.2.16.80",
-        "19.2.168.0",
-        "19.21.6.80",
-        "19.21.68.0",
-        "19.216.8.0",
-        "192.1.6.80",
-        "192.1.68.0",
-        "192.16.8.0",
-    ]
-    actual = validIPAddresses(input)
-    print(actual)
-    assert actual == expected
+    print(is_palindrome("abcdcba"))
