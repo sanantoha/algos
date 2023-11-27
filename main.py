1194cb3a3c7e17ffdbbf55eebc1e4cb771b8e4aa
sanantoha <sanantoha@gmail.com>
2023-11-27 08:46:56 +0400

    +1 tasks task 27.11.2023

--- main.py
+++ main.py
@@ -191,11 +191,16 @@ tasks_done = [
     'sorted_squared_array.py',
     'balanced_brackets.py',
     'valid_ip_addresses.py',
-    'palindrom_check.py'
+    'palindrom_check.py',
+    'longest_increasing_subsequence.py'
 ]
 
 
-def searchInSortedMatrix(matrix, target):
+def longestIncreasingSubsequence(arr):
+    pass
+
+
+def longestIncreasingSubsequence1(arr):
     pass
 
 
@@ -204,13 +209,13 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    matrix = [
-        [1, 4, 7, 12, 15, 1000],
-        [2, 5, 19, 31, 32, 1001],
-        [3, 8, 24, 33, 35, 1002],
-        [40, 41, 42, 44, 45, 1003],
-        [99, 100, 103, 106, 128, 1004],
-    ]
-    actual = searchInSortedMatrix(matrix, 44)
+    input = [5, 7, -24, 12, 10, 2, 3, 12, 5, 6, 35]
+    expected = [-24, 2, 3, 5, 6, 35]
+
+    actual = longestIncreasingSubsequence(input)
     print(actual)
-    assert actual == [3, 3]
+    assert actual == expected
+
+    actual = longestIncreasingSubsequence1(input)
+    print(actual)
+    assert actual == expected
