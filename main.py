894b0ceb5b6a272cb2355a9c6a8a27aa3a8a507d
sanantoha <sanantoha@gmail.com>
2023-12-17 10:33:16 +0400

    +1 tasks task 17.12.2023

--- main.py
+++ main.py
@@ -207,16 +207,18 @@ tasks_done = [
     'search_in_sorted_matrix.py',
     'optimal_freelancing.py',
     'zigzag_traverse.py'
-    # 'binary_search.py'
+    'binary_search.py'
 ]
 
 
-def zigzagTraverse(matrix):
+def binary_search(arr, target):
     pass
 
 
 if __name__ == '__main__':
-    test = [[1, 3, 4, 10], [2, 5, 9, 11], [6, 8, 12, 15], [7, 13, 14, 16]]
-    actual = zigzagTraverse(test)
-    print(actual)
-    assert actual == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    remain = list(set(tasks) - set(tasks_done))
+    random.shuffle(remain)
+    print(remain)
+
+    array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+    print(binary_search(array, 60))
