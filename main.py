213459b4a350296cce8c927a3343a5a00b4e9bb4
sanantoha <sanantoha@gmail.com>
2023-12-25 08:52:59 +0400

    +1 tasks task 25.12.2023

--- main.py
+++ main.py
@@ -212,21 +212,17 @@ tasks_done = [
     'powerset.py',
     'best_seats.py',
     'word_ladder_ii.py',
-    'group_anagrams.py'
+    'group_anagrams.py',
+    'longest_common_subsequence.py'
 ]
 
 
-def groupAnagrams(words):
-    return None
+def longestCommonSubsequence(str1, str2):
+    pass
 
 
-def compare(expected, output):
-    if len(expected) == 0:
-        assert output == expected
-        return
-    assert len(expected) == len(output)
-    for group in expected:
-        assert sorted(group) in output
+def longestCommonSubsequence1(str1, str2):
+    pass
 
 
 if __name__ == '__main__':
@@ -234,11 +230,10 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
-    expected = [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
-    output = list(map(lambda x: sorted(x), groupAnagrams(words)))
-    # expected = list(map(lambda x: sorted(x), expected))
-
+    output = longestCommonSubsequence("ZXVVYZW", "XKYKZPW")
     print(output)
-    print(expected)
-    compare(expected, output)
+    assert output == ["X", "Y", "Z", "W"]
+
+    output = longestCommonSubsequence1("ZXVVYZW", "XKYKZPW")
+    print(output)
+    assert output == ["X", "Y", "Z", "W"]
