941af25df6c65aecd65568c2817bf8fd3b89d663
sanantoha <sanantoha@gmail.com>
2024-02-13 07:17:34 +0400

    +1 tasks task 13.02.2024

--- main.py
+++ main.py
@@ -246,11 +246,14 @@ tasks_done = [
     'common_characters.py',
     'largest_range.py',
     'find_nodes_distance_k.py',
-    'stable_internships.py'
+    'stable_internships.py',
+    'kruskals_algorithm.py',
+    'phone_number_mnemonic.py'
 ]
 
 
-def kruskalsAlgorithm(graph):
+def phoneNumberMnemonics(phoneNumber):
+    letters = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
     pass
 
 
@@ -259,19 +262,8 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    input = [
-        [[1, 3], [2, 5]],
-        [[0, 3], [2, 10], [3, 12]],
-        [[0, 5], [1, 10]],
-        [[1, 12]]
-    ]
-
-    expected = [
-        [[1, 3], [2, 5]],
-        [[0, 3], [3, 12]],
-        [[0, 5]],
-        [[1, 12]]
-    ]
-    actual = kruskalsAlgorithm(input)
-    print(actual, "     =    ", expected)
+    phoneNumber = "1905"
+    expected = ["1w0j", "1w0k", "1w0l", "1x0j", "1x0k", "1x0l", "1y0j", "1y0k", "1y0l", "1z0j", "1z0k", "1z0l"]
+    actual = phoneNumberMnemonics(phoneNumber)
+    print(actual)
     assert actual == expected
