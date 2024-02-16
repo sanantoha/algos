7445ff30f02c0db028ddfd2a269acb6c5688cba5
sanantoha <sanantoha@gmail.com>
2024-02-16 07:40:41 +0400

    +1 tasks task 16.02.2024

--- main.py
+++ main.py
@@ -250,36 +250,13 @@ tasks_done = [
     'kruskals_algorithm.py',
     'phone_number_mnemonic.py',
     'three_number_sum.py',
-    'sum_of_linked_lists.py'
+    'sum_of_linked_lists.py',
+    'run_length_encoding.py'
 ]
 
-class LinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
 
-class LinkedList(LinkedList):
-    def addMany(self, values):
-        current = self
-        while current.next is not None:
-            current = current.next
-        for value in values:
-            current.next = LinkedList(value)
-            current = current.next
-        return self
-
-
-def getNodesInArray(output):
-    nodes = []
-    current = output
-    while current is not None:
-        nodes.append(current.value)
-        current = current.next
-    return nodes
-
-
-def sumOfLinkedLists(l1, l2):
-    return None
+def run_length_encoding(string):
+    pass
 
 
 if __name__ == '__main__':
@@ -287,9 +264,4 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    ll1 = LinkedList(2).addMany([4, 7, 1])
-    ll2 = LinkedList(9).addMany([4, 5])
-    expected = LinkedList(1).addMany([9, 2, 2])
-    actual = sumOfLinkedLists(ll1, ll2)
-    print(getNodesInArray(actual))
-    assert getNodesInArray(actual) == getNodesInArray(expected)
+    print(run_length_encoding("AAAAAAAAAAAAABBCCCCDD") == "9A4A2B4C2D")
