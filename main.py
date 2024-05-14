049d6b68ab48ecc05541e473df5b227d8b40f653
sanantoha <sanantoha@gmail.com>
2024-05-14 07:26:32 +0400

    +1 tasks task 14.05.2024

--- main.py
+++ main.py
@@ -189,12 +189,12 @@ tasks_done = [
     'sorted_squared_array.py',
     'median_of_two_sorted_arrays.py',
     'majority_element.py',
-    'phone_number_mnemonic.py'
+    'phone_number_mnemonic.py',
+    'spiral_matrix_traverse.py'
 ]
 
 
-def phoneNumberMnemonics(phoneNumber):
-    letters = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+def spiral_matrix_traverse(matrix):
     pass
 
 
@@ -203,8 +203,8 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    phoneNumber = "1905"
-    expected = ["1w0j", "1w0k", "1w0l", "1x0j", "1x0k", "1x0l", "1y0j", "1y0k", "1y0l", "1z0j", "1z0k", "1z0l"]
-    actual = phoneNumberMnemonics(phoneNumber)
-    print(actual)
-    assert actual == expected
+    matrix = [[1, 2, 3, 4],
+              [12, 13, 14, 5],
+              [11, 16, 15, 6],
+              [10, 9, 8, 7]]
+    print(spiral_matrix_traverse(matrix))
