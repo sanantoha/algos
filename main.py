ffab64fe9dc9e48382f37b2449c8d7a6373e78b7
AntonSakharovAE <anton@alefeducation.com>
2025-02-28 09:00:37 +0400

    +1 task completed 28.02.2025

--- main.py
+++ main.py
@@ -172,13 +172,12 @@ tasks_done = [
     'find_closest_value_in_bst.py',
     'product_sum.py',
     'array_of_products.py',
-    'find_kth_largest_value_in_bst.py'
+    'find_kth_largest_value_in_bst.py',
+    'kadanes_distance.py'
 ]
 
-from tree.BST import BST
 
-
-def find_kth_largest_value_in_bst(root, k):
+def kadanes_distance(arr):
     pass
 
 
@@ -187,17 +186,4 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    root = BST(15)
-    root.left = BST(5)
-    root.left.left = BST(2)
-    root.left.left.left = BST(1)
-    root.left.left.right = BST(3)
-    root.left.right = BST(5)
-    root.right = BST(20)
-    root.right.left = BST(17)
-    root.right.right = BST(22)
-    k = 3
-    expected = 17
-    actual = find_kth_largest_value_in_bst(root, k)
-    print(actual)
-    assert actual == expected
+    assert kadanes_distance([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]) == 19
