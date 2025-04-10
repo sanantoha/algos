5fabd9b1ac6badf3c33fc22f18d5da6caae8e097
AntonSakharovAE <anton@alefeducation.com>
2025-04-10 06:43:46 +0400

    +1 task completed 10.04.2025

--- main.py
+++ main.py
@@ -209,11 +209,15 @@ tasks_done = [
     'insertion_sort.py',
     'max_sum_increasing_subsequence.py',
     'find_three_largest_numbers.py',
-    'zero_sum_subarray.py'
+    'zero_sum_subarray.py',
+    'validate_bst.py'
 ]
 
 
-def zeroSumSubarray(arr):
+from tree.BST import BST
+
+
+def validate_bst(root):
     pass
 
 
@@ -222,8 +226,14 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    input = [4, 2, -1, -1, 3]
-    expected = True
-    actual = zeroSumSubarray(input)
-    print(actual)
-    assert actual == expected
+    root = BST(10)
+    root.left = BST(5)
+    root.left.left = BST(2)
+    root.left.left.left = BST(1)
+    root.left.right = BST(5)
+    root.right = BST(15)
+    root.right.left = BST(13)
+    root.right.left.right = BST(14)
+    root.right.right = BST(22)
+    print(validate_bst(root))
+    assert validate_bst(root)
