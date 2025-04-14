9ffa200c1ee3354c43534699eadc9513e5842c85
AntonSakharovAE <anton@alefeducation.com>
2025-04-14 06:50:56 +0400

    +1 task completed 14.04.2025

--- main.py
+++ main.py
@@ -212,11 +212,12 @@ tasks_done = [
     'zero_sum_subarray.py',
     'validate_bst.py',
     'sort_stack.py',
-    'valid_ip_addresses.py'
+    'valid_ip_addresses.py',
+    'non_constructible_changes.py'
 ]
 
 
-def validIPAddresses(string):
+def non_constructible_changes(coins):
     pass
 
 
@@ -225,20 +226,5 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    input = "1921680"
-    expected = [
-        "1.9.216.80",
-        "1.92.16.80",
-        "1.92.168.0",
-        "19.2.16.80",
-        "19.2.168.0",
-        "19.21.6.80",
-        "19.21.68.0",
-        "19.216.8.0",
-        "192.1.6.80",
-        "192.1.68.0",
-        "192.16.8.0",
-    ]
-    actual = validIPAddresses(input)
-    print(actual)
-    assert actual == expected
+    coins = [5, 7, 1, 1, 2, 3, 22]
+    print(non_constructible_changes(coins))
