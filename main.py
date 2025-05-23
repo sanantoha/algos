a9ebdc6042a87930b012217b7cb05b38d7908ab5
AntonSakharovAE <anton@alefeducation.com>
2025-05-23 06:59:51 +0400

    +1 task completed 23.05.2025

--- main.py
+++ main.py
@@ -235,21 +235,13 @@ tasks_done = [
     'caesar_cipher_encryptor.py',
     'river_sizes.py',
     'group_anagrams.py',
-    'depth_first_search.py'
+    'depth_first_search.py',
+    'knapsack_problem.py'
 ]
 
 
-class Node:
-    def __init__(self, name):
-        self.children = []
-        self.name = name
-
-    def add_child(self, name):
-        self.children.append(Node(name))
-        return self
-
-    def depth_first_search(self, array):
-        pass
+def knapsackProblem(items, capacity):
+    pass
 
 
 if __name__ == '__main__':
@@ -257,10 +249,9 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    graph = Node("A")
-    graph.add_child("B").add_child("C").add_child("D")
-    graph.children[0].add_child("E").add_child("F")
-    graph.children[2].add_child("G").add_child("H")
-    graph.children[0].children[1].add_child("I").add_child("J")
-    graph.children[2].children[0].add_child("K")
-    assert graph.depth_first_search([]) == ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"]
+    items = [[1, 2], [4, 3], [5, 6], [6, 7]]
+    capacity = 10
+    expected = [10, [1, 3]]
+    actual = knapsackProblem(items, capacity)
+    print(actual)
+    assert actual == expected
