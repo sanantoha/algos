f2c0358737a56b2f4f0c43110ffa41e88aa520e4
AntonSakharovAE <anton@alefeducation.com>
2025-07-10 07:44:23 +0400

    +1 task completed 10.07.2025

--- main.py
+++ main.py
@@ -256,16 +256,44 @@ tasks_done = [
     'apartment_hunting.py',
     'a_star_algorithm.py',
     'lagest_island.py',
-    'max_subset_sum_no_adjucent.py'
+    'max_subset_sum_no_adjucent.py',
+    'longest_common_subsequence.py',
+    'bst_construction.py'
 ]
 
 
-def max_subset_sum_no_adjucent(arr):
-    pass
+# Do not edit the class below except for
+# the insert, contains, and remove methods.
+# Feel free to add new properties and methods
+# to the class.
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
 
-def max_subset_sum_no_adjucent1(arr):
-    pass
+    def insert_rec(self, value):
+        return None
+
+
+    def insert(self, value):
+        return None
+
+
+    def contains(self, value):
+        return False
+
+
+    def contains_rec(self, value):
+        return False
+
+
+    def remove(self, value):
+        return None
+
+    def remove_rec(self, value):
+        return None
 
 
 if __name__ == '__main__':
@@ -273,8 +301,41 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    assert max_subset_sum_no_adjucent([75, 105, 120, 75, 90, 135]) == 330
+    root = BST(10)
+    root.left = BST(5)
+    root.left.left = BST(2)
+    root.left.left.left = BST(1)
+    root.left.right = BST(5)
+    root.right = BST(15)
+    root.right.left = BST(13)
+    root.right.left.right = BST(14)
+    root.right.right = BST(22)
 
-    assert max_subset_sum_no_adjucent1([75, 105, 120, 75, 90, 135]) == 330
-    print(max_subset_sum_no_adjucent1([75, 105, 120, 75, 90, 135]))
+    root.insert(12)
+    assert (root.right.left.left.value == 12)
+
+    root.remove(10)
+    assert (not root.contains(10))
+    assert (root.value == 12)
+
+    assert (root.contains(15))
+
+    root1 = BST(10)
+    root1.left = BST(5)
+    root1.left.left = BST(2)
+    root1.left.left.left = BST(1)
+    root1.left.right = BST(5)
+    root1.right = BST(15)
+    root1.right.left = BST(13)
+    root1.right.left.right = BST(14)
+    root1.right.right = BST(22)
+
+    root1.insert_rec(12)
+    assert (root1.right.left.left.value == 12)
+
+    root1.remove_rec(10)
+    assert (not root1.contains_rec(10))
+    assert (root1.value == 12)
+
+    assert (root1.contains_rec(15))
 
