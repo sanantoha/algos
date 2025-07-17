2e3f9d228358d6b1372e919d6d3aa574e7dff5cd
AntonSakharovAE <anton@alefeducation.com>
2025-07-17 07:07:51 +0400

    +1 task completed 17.07.2025

--- main.py
+++ main.py
@@ -258,42 +258,38 @@ tasks_done = [
     'lagest_island.py',
     'max_subset_sum_no_adjucent.py',
     'longest_common_subsequence.py',
-    'bst_construction.py'
+    'bst_construction.py',
+    'serialize_and_deserialize_n_ary_tree.py'
 ]
 
 
-# Do not edit the class below except for
-# the insert, contains, and remove methods.
-# Feel free to add new properties and methods
-# to the class.
-class BST:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+    def __repr__(self):
+        return f"Node({self.val} {self.children})"
 
 
-    def insert_rec(self, value):
-        return None
+class Codec:
+
+    def serialize(self, root):
+        pass
 
 
-    def insert(self, value):
-        return None
+    def deserialize(self, data):
+        pass
 
 
-    def contains(self, value):
-        return False
+class Codec1:
+
+    def serialize(self, root):
+        pass
 
 
-    def contains_rec(self, value):
-        return False
-
-
-    def remove(self, value):
-        return None
-
-    def remove_rec(self, value):
-        return None
+    def deserialize(self, data):
+        pass
 
 
 if __name__ == '__main__':
@@ -301,41 +297,19 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    root = BST(10)
-    root.left = BST(5)
-    root.left.left = BST(2)
-    root.left.left.left = BST(1)
-    root.left.right = BST(5)
-    root.right = BST(15)
-    root.right.left = BST(13)
-    root.right.left.right = BST(14)
-    root.right.right = BST(22)
+    root = Node(11, [Node(33, [Node(55), Node(6666)]), Node(22), Node(30)])
+    print(root)
+    codec = Codec()
+    str = codec.serialize(root)
+    print(str)
 
-    root.insert(12)
-    assert (root.right.left.left.value == 12)
+    tree = codec.deserialize(str)
+    print(tree)
 
-    root.remove(10)
-    assert (not root.contains(10))
-    assert (root.value == 12)
+    codec1 = Codec1()
+    str = codec1.serialize(root)
+    print(str)
 
-    assert (root.contains(15))
-
-    root1 = BST(10)
-    root1.left = BST(5)
-    root1.left.left = BST(2)
-    root1.left.left.left = BST(1)
-    root1.left.right = BST(5)
-    root1.right = BST(15)
-    root1.right.left = BST(13)
-    root1.right.left.right = BST(14)
-    root1.right.right = BST(22)
-
-    root1.insert_rec(12)
-    assert (root1.right.left.left.value == 12)
-
-    root1.remove_rec(10)
-    assert (not root1.contains_rec(10))
-    assert (root1.value == 12)
-
-    assert (root1.contains_rec(15))
+    tree = codec1.deserialize(str)
+    print(tree)
 
