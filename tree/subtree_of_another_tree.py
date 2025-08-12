45681760d4346e0546f164cae7253559431bd34e
AntonSakharovAE <anton@alefeducation.com>
2025-08-12 07:05:08 +0400

    +1 task completed 12.08.2025

---
+++ tree/subtree_of_another_tree.py
@@ -0,0 +1,45 @@
+from BinaryTree import BinaryTree
+
+
+def is_subtree(root, sub_root):
+    return False
+
+
+if __name__ == '__main__':
+    root1 = BinaryTree(3)
+    root1.left = BinaryTree(4)
+    root1.left.left = BinaryTree(1)
+    root1.left.right = BinaryTree(2)
+    root1.right = BinaryTree(5)
+
+    sub_tree = BinaryTree(4)
+    sub_tree.left = BinaryTree(1)
+    sub_tree.right = BinaryTree(2)
+
+    res = is_subtree(root1, sub_tree)
+    print(res)  # True
+    assert res
+
+    root2 = BinaryTree(3)
+    root2.left = BinaryTree(4)
+    root2.left.left = BinaryTree(1)
+    root2.left.right = BinaryTree(2)
+    root2.left.right.left = BinaryTree(0)
+    root2.right = BinaryTree(5)
+
+    res = is_subtree(root2, sub_tree)
+    print(res)  # False
+    assert res
+
+    root3 = BinaryTree(4)
+    root3.left = BinaryTree(4)
+    root3.left.left = BinaryTree(4)
+    root3.left.left.left = BinaryTree(4)
+    root3.left.left.left.left = BinaryTree(4)
+    root3.left.left.left.left.left = BinaryTree(1)
+    root3.left.left.left.left.right = BinaryTree(2)
+
+    res = is_subtree(root3, sub_tree)
+    print(res) # True
+    assert res
+
