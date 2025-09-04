52327cd4be7ac2fd4971793073285009049ca6c8
AntonSakharovAE <anton@alefeducation.com>
2025-09-04 07:23:34 +0400

    +1 task completed 04.09.2025

--- main.py
+++ main.py
@@ -169,12 +169,12 @@ tasks_done = [
     'minimum_passes_of_matrix.py',
     'disk_stacking.py',
     'find_successor.py',
-    'subtree_of_another_tree.py'
+    'subtree_of_another_tree.py',
+    'array_of_products.py'
 ]
 
-from tree.BinaryTree import BinaryTree
 
-def is_subtree(root, sub_root):
+def array_of_products(arr):
     pass
 
 
@@ -183,39 +183,4 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    root1 = BinaryTree(3)
-    root1.left = BinaryTree(4)
-    root1.left.left = BinaryTree(1)
-    root1.left.right = BinaryTree(2)
-    root1.right = BinaryTree(5)
-
-    sub_tree = BinaryTree(4)
-    sub_tree.left = BinaryTree(1)
-    sub_tree.right = BinaryTree(2)
-
-    res = is_subtree(root1, sub_tree)
-    print(res)  # True
-    assert res
-
-    root2 = BinaryTree(3)
-    root2.left = BinaryTree(4)
-    root2.left.left = BinaryTree(1)
-    root2.left.right = BinaryTree(2)
-    root2.left.right.left = BinaryTree(0)
-    root2.right = BinaryTree(5)
-
-    res = is_subtree(root2, sub_tree)
-    print(res)  # False
-    assert not res
-
-    root3 = BinaryTree(4)
-    root3.left = BinaryTree(4)
-    root3.left.left = BinaryTree(4)
-    root3.left.left.left = BinaryTree(4)
-    root3.left.left.left.left = BinaryTree(4)
-    root3.left.left.left.left.left = BinaryTree(1)
-    root3.left.left.left.left.right = BinaryTree(2)
-
-    res = is_subtree(root3, sub_tree)
-    print(res)  # True
-    assert res
+    print(array_of_products([1, 2, 3, 4]))
