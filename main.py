cc41370ec806f3cadcf6df5d0c058a1602183902
AntonSakharovAE <anton@alefeducation.com>
2025-09-20 12:31:32 +0400

    +1 task completed 20.09.2025

--- main.py
+++ main.py
@@ -187,10 +187,14 @@ tasks_done = [
     'river_sizes.py',
     'find_pivot_index.py',
     'zigzag_traverse.py',
+    'invert_binary_tree.py',
 ]
 
 
-def zigzag_traverse(matrix):
+from tree.BinaryTree import BinaryTree
+
+
+def invert_binary_tree(root):
     pass
 
 
@@ -199,7 +203,12 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    test = [[1, 3, 4, 10], [2, 5, 9, 11], [6, 8, 12, 15], [7, 13, 14, 16]]
-    actual = zigzag_traverse(test)
-    print(actual)
-    assert actual == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    tree = BinaryTree(1).insert([2, 3, 4, 5, 6, 7, 8, 9])
+    inverted_tree = BinaryTree(1).inverted_insert([2, 3, 4, 5, 6, 7, 8, 9])
+    invert_binary_tree(tree)
+    assert tree.__eq__(inverted_tree)
+
+    tree1 = BinaryTree(1).insert([2, 3, 4, 5, 6, 7, 8, 9])
+    inverted_tree1 = BinaryTree(1).inverted_insert([2, 3, 4, 5, 6, 7, 8, 9])
+    invert_binary_tree_iter(tree1)
+    assert tree1.__eq__(inverted_tree1)
