88becc286aed6346418d3941fab4212163e4666f
AntonSakharovAE <anton@alefeducation.com>
2025-10-09 06:40:09 +0400

    +1 task completed 09.10.2025

--- main.py
+++ main.py
@@ -205,33 +205,13 @@ tasks_done = [
     'run_length_encoding.py',
     'remove_islands.py',
     'kruskals_algorithm.py',
-    'get_youngest_common_ancestor.py'
+    'get_youngest_common_ancestor.py',
+    'word_ladder.py'
 ]
 
 
-# This is an input class. Do not edit.
-class AncestralTree:
-    def __init__(self, name):
-        self.name = name
-        self.ancestor = None
-
-
-class AncestralTree(AncestralTree):
-    def addDescendants(self, *descendants):
-        for descendant in descendants:
-            descendant.ancestor = self
-
-
-def new_trees():
-    ancestralTrees = {}
-    for letter in list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-        ancestralTrees[letter] = AncestralTree(letter)
-    return ancestralTrees
-
-
-# O(h) time | O(1) space
-def get_youngest_common_ancestor(top_ancestor, descendant_one, descendant_two):
-    return None
+def ladderLength(beginWord, endWord, wordList):
+    pass
 
 
 if __name__ == '__main__':
@@ -239,11 +219,9 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    trees = new_trees()
-    trees["A"].addDescendants(trees["B"], trees["C"])
-    trees["B"].addDescendants(trees["D"], trees["E"])
-    trees["D"].addDescendants(trees["H"], trees["I"])
-    trees["C"].addDescendants(trees["F"], trees["G"])
-
-    yca = get_youngest_common_ancestor(trees["A"], trees["E"], trees["I"])
-    assert yca == trees["B"]
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+    actual = ladderLength(beginWord, endWord, wordList)
+    print(actual)
+    assert actual == 5
