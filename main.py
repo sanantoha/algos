d2c96e74513029d5a6c96c5238dbb0f90f95afd4
sanantoha <sanantoha@gmail.com>
2026-01-10 08:29:27 +0400

    +1 task added.

--- main.py
+++ main.py
@@ -150,19 +150,12 @@ tasks_done = [
     'find_three_largest_numbers.py',
     'custom_pow.py',
     'number_of_islands.py',
-    'knapsack_problem.py'
+    'knapsack_problem.py',
+    'first_non_repeating_character.py',
 ]
 
 
-def relative_sort_array(arr1, arr2):
-    pass
-
-
-def relative_sort_array_hashmap(arr1, arr2):
-    pass
-
-
-def relative_sort_array_counting_sort(arr1, arr2):
+def first_non_repeating_character(string):
     pass
 
 
@@ -171,32 +164,4 @@ if __name__ == '__main__':
     random.shuffle(remain)
     print(remain)
 
-    arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19]
-    arr2 = [2, 1, 4, 3, 9, 6]
-
-    res = relative_sort_array(arr1, arr2)
-    res1 = relative_sort_array_hashmap(arr1, arr2)
-    res2 = relative_sort_array_counting_sort(arr1, arr2)
-
-    print(res)
-    print(res1)
-    print(res2)
-    assert res == [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]
-    assert res1 == [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]
-    assert res2 == [2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]
-
-    # ==========================================
-
-    arr1 = [28, 6, 22, 8, 44, 17]
-    arr2 = [22, 28, 8, 6]
-
-    res = relative_sort_array(arr1, arr2)
-    res1 = relative_sort_array_hashmap(arr1, arr2)
-    res2 = relative_sort_array_counting_sort(arr1, arr2)
-
-    print(res)
-    print(res1)
-    print(res2)
-    assert res == [22, 28, 8, 6, 17, 44]
-    assert res1 == [22, 28, 8, 6, 17, 44]
-    assert res2 == [22, 28, 8, 6, 17, 44]
+    print(first_non_repeating_character("abcdcaf") == 1)
---
+++ tests/unit/strings/test_first_non_repeating_character.py
@@ -0,0 +1,5 @@
+from strings.first_non_repeating_character import first_non_repeating_character
+
+
+def test_first_non_repeating_character():
+    assert first_non_repeating_character("abcdcaf") == 1
